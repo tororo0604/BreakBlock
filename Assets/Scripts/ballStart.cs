09766627198e24cf4219ef9df40859148cccd930
tororo0604 <kamepaper0604@gmail.com>
2019-12-14 22:27:30 +0900

    ball space

--- Assets/Scripts/ballStart.cs
+++ Assets/Scripts/ballStart.cs
@@ -4,29 +4,33 @@ using UnityEngine;
 
 public class ballStart : MonoBehaviour {
 
-    public int ballCount = 0;
+    public int ballCount;
 
     // Start is called before the first frame update
     void Start () {
         //gameObject.GetComponent<Rigidbody> ().AddForce (1000f, 0f, 1000f);
-        gameObject.GetComponent<Rigidbody> ().velocity = new Vector3 (10f, 0f, 10f);
+        gameObject.GetComponent<Rigidbody> ().velocity
+         = new Vector3 ((Random.value - 0.5f) * 20f, 0f, 10f);
+        ballCount = 0;
     }
 
     // Update is called once per frame
     void Update () {
         Vector3 tmp = this.transform.position;
-        if(tmp.y < -1){
-            Destroy(gameObject);
+        if (tmp.y < -1) {
+            Destroy (gameObject);
+            ballCount -= 1;
         }
 
-        if(Input.GetKeyDown(KeyCode.Space)){
-            
+        if (Input.GetKeyDown (KeyCode.Space)) {
+            ballCount += 1;
         }
     }
 
     void OnCollisionEnter (Collision col) {
         if (col.gameObject.tag == "deadZone") {
             Destroy (this.gameObject);
+            ballCount -= 1;
         }
     }
 
